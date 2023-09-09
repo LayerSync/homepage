b658e00860bbeec45ee08649751fd250c93fe841
LayerSync Dev <144031631+layersync-dev@users.noreply.github.com>
2023-09-09 06:14:06 -0700

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,10 @@
+var owl = $(".owl-carousel");
+owl.owlCarousel({
+  items: 6,
+  loop: true,
+  nav: true,
+  margin: 10,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
+});
